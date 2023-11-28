fcca28489edccca286f7c2142ba19fbc4ac6b0fd
Thuan2003 <128351834+Thuan2003@users.noreply.github.com>
2023-11-28 14:41:46 +0700

    Update MyConstants.js

--- server/utils/MyConstants.js
+++ server/utils/MyConstants.js
@@ -1,11 +1,11 @@
 const MyConstants = {
-  DB_SERVER: 'cluster0.cq7ju1z.mongodb.net',
-  DB_USER: 'Takotoko',
-  DB_PASS: 'Thuan2003',
+  DB_SERVER: 'cluster0.hldzbji.mongodb.net',
+  DB_USER: 'Khang181203',
+  DB_PASS: '181203',
   DB_DATABASE: 'shoppingonline',
-  JWT_SECRET: 'Thuan2003',
+  JWT_SECRET: 'BaeKag',
   JWT_EXPIRES: '36000000', // in milliseconds
-  EMAIL_USER: 'giathuan210@gmail.com', // gmail service
-  EMAIL_PASS: 'wdhr twfk fxcp slns'
+  EMAIL_USER: 'minhkhang122003@gmail.com', // gmail service
+  EMAIL_PASS: 'znzh uwjp sagq ugnx'
 };
 module.exports = MyConstants; 
