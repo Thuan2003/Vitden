bd96df58fec89ce052ba71a752077ae968a9ab82
Thuan2003 <128351834+Thuan2003@users.noreply.github.com>
2023-11-29 19:49:20 +0700

    Update MyordersComponent.js

--- client-customer/src/components/MyordersComponent.js
+++ client-customer/src/components/MyordersComponent.js
@@ -1,127 +1,127 @@
-import axios from 'axios';
-import React, { Component } from 'react';
-import { Navigate } from 'react-router-dom';
-import MyContext from '../contexts/MyContext';
+  import axios from 'axios';
+  import React, { Component } from 'react';
+  import { Navigate } from 'react-router-dom';
+  import MyContext from '../contexts/MyContext';
 
-class Myorders extends Component {
-  static contextType = MyContext; // using this.context to access global state
+  class Myorders extends Component {
+    static contextType = MyContext; // using this.context to access global state
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      orders: [],
-      order: null
-    };
-  }
+    constructor(props) {
+      super(props);
+      this.state = {
+        orders: [],
+        order: null
+      };
+    }
 
-  render() {
-    if (this.context.token === '') return <Navigate replace to="/login" />;
+    render() {
+      if (this.context.token === '') return <Navigate replace to="/login" />;
 
-    const containerStyle = {
-      textAlign: 'center',
-      border: '2px solid #3498db',
-      borderRadius: '10px',
-      padding: '20px',
-      boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-      backgroundColor: '#fff',
-      maxWidth: '800px', // Adjust the maximum width as needed
-      margin: 'auto', // Center the container
-      marginBottom: '20px', // Add some margin at the bottom
-    };
+      const containerStyle = {
+        textAlign: 'center',
+        border: '2px solid #3498db',
+        borderRadius: '10px',
+        padding: '20px',
+        boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+        backgroundColor: '#fff',
+        maxWidth: '800px', // Adjust the maximum width as needed
+        margin: 'auto', // Center the container
+        marginBottom: '20px', // Add some margin at the bottom
+      };
 
-    const headingStyle = {
-      color: '#3498db'
-    };
+      const headingStyle = {
+        color: '#3498db'
+      };
 
-    const orders = this.state.orders.map((item) => {
-      return (
-        <tr key={item._id} className="datatable" onClick={() => this.trItemClick(item)}>
-          <td>{item._id}</td>
-          <td>{new Date(item.cdate).toLocaleString()}</td>
-          <td>{item.customer.name}</td>
-          <td>{item.customer.phone}</td>
-          <td>{item.total}</td>
-          <td>{item.status}</td>
-        </tr>
-      );
-    });
-
-    if (this.state.order) {
-      var items = this.state.order.items.map((item, index) => {
+      const orders = this.state.orders.map((item) => {
         return (
-          <tr key={item.product._id} className="datatable">
-            <td>{index + 1}</td>
-            <td>{item.product._id}</td>
-            <td>{item.product.name}</td>
-            <td><img src={"data:image/jpg;base64," + item.product.image} width="70px" height="70px" alt="" /></td>
-            <td>{item.product.price}</td>
-            <td>{item.quantity}</td>
-            <td>{item.product.price * item.quantity}</td>
+          <tr key={item._id} className="datatable" onClick={() => this.trItemClick(item)}>
+            <td>{item._id}</td>
+            <td>{new Date(item.cdate).toLocaleString()}</td>
+            <td>{item.customer.name}</td>
+            <td>{item.customer.phone}</td>
+            <td>{item.total}</td>
+            <td>{item.status}</td>
           </tr>
         );
       });
-    }
 
-    return (
-      <div style={containerStyle}>
-        <h2 style={headingStyle}>ORDER LIST</h2>
-        <table className="datatable" border="1">
-          <tbody>
-            <tr className="datatable">
-              <th>ID</th>
-              <th>Creation date</th>
-              <th>Cust.name</th>
-              <th>Cust.phone</th>
-              <th>Total</th>
-              <th>Status</th>
+      if (this.state.order) {
+        var items = this.state.order.items.map((item, index) => {
+          return (
+            <tr key={item.product._id} className="datatable">
+              <td>{index + 1}</td>
+              <td>{item.product._id}</td>
+              <td>{item.product.name}</td>
+              <td><img src={"data:image/jpg;base64," + item.product.image} width="70px" height="70px" alt="" /></td>
+              <td>{item.product.price}</td>
+              <td>{item.quantity}</td>
+              <td>{item.product.price * item.quantity}</td>
             </tr>
-            {orders}
-          </tbody>
-        </table>
+          );
+        });
+      }
 
-        {this.state.order ?
-          <div style={containerStyle}>
-            <h2 style={headingStyle}>ORDER DETAIL</h2>
-            <table className="datatable" border="1">
-              <tbody>
-                <tr className="datatable">
-                  <th>No.</th>
-                  <th>Prod.ID</th>
-                  <th>Prod.name</th>
-                  <th>Image</th>
-                  <th>Price</th>
-                  <th>Quantity</th>
-                  <th>Amount</th>
-                </tr>
-                {items}
-              </tbody>
-            </table>
-          </div>
-          : <div />}
-      </div>
-    );
-  }
+      return (
+        <div style={containerStyle}>
+          <h2 style={headingStyle}>ORDER LIST</h2>
+          <table className="datatable" border="1">
+            <tbody>
+              <tr className="datatable">
+                <th>ID</th>
+                <th>Creation date</th>
+                <th>Cust.name</th>
+                <th>Cust.phone</th>
+                <th>Total</th>
+                <th>Status</th>
+              </tr>
+              {orders}
+            </tbody>
+          </table>
 
-  componentDidMount() {
-    if (this.context.customer) {
-      const cid = this.context.customer._id;
-      this.apiGetOrdersByCustID(cid);
+          {this.state.order ?
+            <div style={containerStyle}>
+              <h2 style={headingStyle}>ORDER DETAIL</h2>
+              <table className="datatable" border="1">
+                <tbody>
+                  <tr className="datatable">
+                    <th>No.</th>
+                    <th>Prod.ID</th>
+                    <th>Prod.name</th>
+                    <th>Image</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Amount</th>
+                  </tr>
+                  {items}
+                </tbody>
+              </table>
+            </div>
+            : <div />}
+        </div>
+      );
+    }
+
+    componentDidMount() {
+      if (this.context.customer) {
+        const cid = this.context.customer._id;
+        this.apiGetOrdersByCustID(cid);
+      }
+    }
+
+    // event-handlers
+    trItemClick(item) {
+      this.setState({ order: item });
+    }
+
+    // apis
+    apiGetOrdersByCustID(cid) {
+      const config = { headers: { 'x-access-token': this.context.token } };
+      axios.get('/api/customer/orders/customer/' + cid, config).then((res) => {
+        const result = res.data;
+        this.setState({ orders: result });
+      });
     }
   }
 
-  // event-handlers
-  trItemClick(item) {
-    this.setState({ order: item });
-  }
-
-  // apis
-  apiGetOrdersByCustID(cid) {
-    const config = { headers: { 'x-access-token': this.context.token } };
-    axios.get('/api/customer/orders/customer/' + cid, config).then((res) => {
-      const result = res.data;
-      this.setState({ orders: result });
-    });
-  }
-}
-
-export default Myorders;
+  export default Myorders;
