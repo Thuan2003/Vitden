418bf3ab9046c7fa3f69de0864e2a6c0aef88406
Thuan2003 <128351834+Thuan2003@users.noreply.github.com>
2023-11-29 19:37:30 +0700

    Update LoginComponent.js

--- client-customer/src/components/LoginComponent.js
+++ client-customer/src/components/LoginComponent.js
@@ -9,8 +9,8 @@ class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      txtUsername: '',
-      txtPassword: '',
+      txtUsername: 'thuan2003',
+      txtPassword: '123',
     };
   }
 
